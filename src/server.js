ac65d908d30a739eaafcaa3b06f0d90019e7aeb4
Timo Wang <timo@steaker.com>
2022-01-24 13:01:15 +0800

    fix date issue

--- src/server.js
+++ src/server.js
@@ -3,24 +3,26 @@ const { firestore } = require('./firebase');
 
 async function addDocFunc() {
     try {
-        const newUserDocRef = doc(firestore, 'dev/users/users/someone@gmail.com');
+        const newUserDocRef = doc(firestore, 'dev/users/users', 'someone@gmail.com');
         await setDoc(newUserDocRef, {
             walletAddress: '0x01',
             gamebagAddress: '0x03'
         });
         console.log('newUserDocRef', newUserDocRef);
 
-        const nextHour = new Date() + 3600 * 1000;
-        const newLoginPendingDocRef = doc(firestore, 'dev/users/loginPending/someone@gmail.com');
+        const oneHourLater = new Date(new Date().getTime() + 3600 * 1000).toISOString();
+        const fiveMinsLater = new Date(new Date().getTime() + 300 * 1000).toISOString();
+        const newLoginPendingDocRef = doc(firestore, 'dev/users/loginPending', 'someone@gmail.com');
+
         await setDoc(newLoginPendingDocRef, {
             sendEmailAttempts: 0, // upper limit 5
-            sendEmailAttemptsLockExpiry: nextHour,   // 1 hour later
+            sendEmailAttemptsLockExpiry: oneHourLater,   // 1 hour later
 
             enterCodeAttempts: 0, // upper limit 5
-            enterCodeAttemptsLockExpiry: nextHour,   // 1 hour later
+            enterCodeAttemptsLockExpiry: oneHourLater,   // 1 hour later
 
             entryCode: '123456',
-            entryCodeExpiry: 1234567890  // 5 minutes later
+            entryCodeExpiry: fiveMinsLater  // 5 minutes later
         });
         console.log('newLoginPendingDocRef', newLoginPendingDocRef);
     } catch (error) {
@@ -29,7 +31,7 @@ async function addDocFunc() {
 }
 
 async function queryDocFunc() {
-    const userDocRef = doc(firestore, 'dev/users/users/someone@gmail.com');
+    const userDocRef = doc(firestore, 'dev/users/users', 'someone@gmail.com');
     const userDoc = await getDoc(userDocRef);
     if (userDoc.exists()) {
         console.log('user doc data', userDoc.data());
@@ -39,7 +41,7 @@ async function queryDocFunc() {
     const q = query(userCollectionRef, where('walletAddress', '==', '0x01'));
     const userDocs = await getDocs(q);
     userDocs.forEach((userDoc) => {
-        console.log(userDocs.id, '=>', userDoc.data());
+        console.log(userDoc.id, '=>', userDoc.data());
     })
 }
 
